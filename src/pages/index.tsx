0fc72e932402d2d594fe71adec09303ba2ec4b4c
mk575 <masaki.kato1108@gmail.com>
2021-04-04 17:31:04 +0900

    change JSX from hello to sample()

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,9 @@
+import sample from "./sample";
+
 const Home: React.FC = () => {
   return (
     <div className="flex justify-center items-center flex-col min-h-screen font-mono">
-      Hello Nextjs
+      {sample()}
     </div>
   );
 };
--- src/pages/sample.tsx
+++ src/pages/sample.tsx
@@ -1,18 +1,21 @@
+import React from "react";
 import { useState } from "react";
 
 const sample = () => {
-  const [value, setValuse] = useState<number>(0);
+  const [value, setValue] = useState<number>(0);
 
   return (
     <div className="mt-10 ml-10 flex flex-col">
       <div>
         <span>{`計算結果=${value}`}</span>
       </div>
+
       <div>
-        <input type="text" className="border border-black" />
+        <input type="number" className="border border-black" />
       </div>
+
       <div>
-        <button className="bg-gray-200" onClick={() => setValuse(value + 1)}>
+        <button className="bg-gray-200" onClick={() => setValue(value + 1)}>
           ＋１
         </button>
       </div>
