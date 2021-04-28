35967d43ef5e421f4a1850bf52c120930c267d3d
oshita-n <oshita-n@whispon.com>
2021-04-29 03:46:36 +0900

    ページの変更

--- src/pages/exception1.tsx
+++ src/pages/exception1.tsx
@@ -1,15 +1,22 @@
 import { NextPage } from "next";
 import { Divider } from "../components/shared/Divider";
 import { Button } from "../components/shared/Button";
+import { MainLayouts } from "../layouts/MainLayouts";
+
 
 export const exeption1: NextPage = () => {
   return (
     <div>
-      <Divider/>
-      <img src="exception.png" />
-      <Divider/>
-      <Divider/>
-      <Button button>続けて洋服の写真を撮る</Button>
+      <MainLayouts title="top">
+        <div className="flex justify-center">
+          <img src="exception.png" />
+        </div>
+        <Divider/>
+        <Divider/>
+        <div className="flex justify-center">
+          <Button button>続けて洋服の写真を撮る</Button>
+          </div>
+      </MainLayouts>
     </div>
   );
 };
