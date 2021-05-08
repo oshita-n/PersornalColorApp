d0bebddece8370c46564689590579d9167794306
cti1650 <cti1650@gmail.com>
2021-05-09 05:57:22 +0900

    🔨 FittingColorコンポーネントにデフォルト値を設定

--- src/components/shared/FittingColor.tsx
+++ src/components/shared/FittingColor.tsx
@@ -3,25 +3,27 @@ import cc from "classcat";
 
 type Props = {
   className?: string;
+  imageLink?: string;
   bgColor?: string;
   bodyColor?: string;
 };
 
 export const FittingColor: VFC<Props> = (props) => {
+  const { className, bgColor, bodyColor, imageLink } = props;
   return (
     <div
-      style={{ backgroundColor: props.bgColor }}
+      style={{ backgroundColor: bgColor }}
       className={cc([
         "rounded-full flex flex-col text-white justify-center items-center w-60 h-60 select-none",
         {
-          "bg-gray-300": !props.bgColor,
+          "bg-gray-300": !bgColor,
         },
-        props.className,
+        className,
       ])}
     >
-      <img className="rounded-full object-cover w-24 h-24 flex items-center justify-center" src="https://image-more.hpplus.jp/upload/be3/be3e336c18af51864659bd48c2ca02be.jpg"></img>
+      <img className="rounded-full object-cover w-24 h-24 flex items-center justify-center" src={imageLink}></img>
       <div style={{
-        backgroundColor: props.bodyColor || '#C0C0C0',
+        backgroundColor: bodyColor,
         width: '60%',
         height: '30%',
         borderRadius: '70% 70% 10% 10%'
@@ -29,3 +31,8 @@ export const FittingColor: VFC<Props> = (props) => {
     </div>
   );
 };
+FittingColor.defaultProps = {
+  imageLink: 'https://image-more.hpplus.jp/upload/be3/be3e336c18af51864659bd48c2ca02be.jpg',
+  bodyColor: '#C0C0C0',
+};
+export default FittingColor;
