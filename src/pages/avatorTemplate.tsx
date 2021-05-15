c852d2b5b3fcb17d5b51263b87d5ed5862b2288b
oshita-n　　 <oshita-n@whispon.com>
2021-05-15 21:41:57 +0900

    ✨ 背景までを作成

--- src/pages/avatorTemplate.tsx
+++ src/pages/avatorTemplate.tsx
@@ -1,7 +1,23 @@
 import { NextPage } from "next";
+import { Divider } from "../components/shared/Divider";
+import { Button } from "../components/shared/Button";
+import { MainLayouts } from "../layouts/MainLayouts";
 
 export const avatorTemplate: NextPage = () => {
-  return <div>avatorTemplate</div>;
+  return (
+    <div>
+      <MainLayouts title="top">
+          <div className="flex flex-col items-center">
+          <div className="w-72 h-72 my-4  bg-yellow-200"></div>
+            <p>あなたの顔が認識されました</p>
+            <Divider/>
+            <div>
+              <Button button className="mt-8 mb-10">洋服の写真を撮る</Button>
+            </div>
+          </div>
+      </MainLayouts>
+    </div>
+  );
 };
 
 export default avatorTemplate;
