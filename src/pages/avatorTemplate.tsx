3e8602b91a9fc84423e8af73acd43ab48411b2fa
oshita-n　　 <oshita-n@whispon.com>
2021-05-16 11:15:36 +0900

    🎨 FittingColorコンポーネントの適用

--- src/pages/avatorTemplate.tsx
+++ src/pages/avatorTemplate.tsx
@@ -2,19 +2,20 @@ import { NextPage } from "next";
 import { Divider } from "../components/shared/Divider";
 import { Button } from "../components/shared/Button";
 import { MainLayouts } from "../layouts/MainLayouts";
+import { FittingColor } from "../components/shared/FittingColor";
 
 export const avatorTemplate: NextPage = () => {
   return (
     <div>
       <MainLayouts title="top">
-          <div className="flex flex-col items-center">
-          <div className="w-72 h-72 my-4  bg-yellow-200"></div>
-            <p>あなたの顔が認識されました</p>
-            <Divider/>
-            <div>
-              <Button button className="mt-8 mb-10">洋服の写真を撮る</Button>
-            </div>
+        <div className="flex flex-col items-center">
+          <FittingColor bgColor="#b0cde5" bodyColor="#67a8dd" />
+          <p>あなたの顔が認識されました</p>
+          <Divider/>
+          <div>
+            <Button button className="mt-8 mb-10">洋服の写真を撮る</Button>
           </div>
+        </div>
       </MainLayouts>
     </div>
   );
