c5da70798aebfe357d7deea0c02b9712bb43c7f5
takasaki376 <75880059+takasaki376@users.noreply.github.com>
2021-06-19 19:04:59 +0900

    🔨 API呼び出しを追加

--- src/pages/sample2.tsx
+++ src/pages/sample2.tsx
@@ -1,7 +1,16 @@
-import { useState, VFC } from "react";
+import { useState, useEffect } from "react";
+import type { VFC } from "react";
+import axios from "axios";
+import useSWR from "swr";
 
 // タスクデータの型
 type Task = {
+  id: number;
+  task: string;
+  created_at: string;
+  updated_at: string;
+};
+type EditTask = {
   id: number;
   task: string;
 };
@@ -12,46 +21,48 @@ const initTask = {
   task: "",
 };
 
+const URL = "http://127.0.0.1:8000/api/task/";
+// 検索
+export const taskGet = (url: string) =>
+  axios.get<Task[]>(url).then((res) => res.data);
+// 新規登録
+export const taskPost = (url: string, task: EditTask) =>
+  axios.post<Task>(url, task).then((res) => res.data);
+// 更新
+export const taskPut = (url: string, id: number, task: EditTask) =>
+  axios.put<Task>(`${url}${id}/`, task).then((res) => res.data);
+// 削除
+export const taskDelete = (url: string, id: number) =>
+  axios.delete(`${url}${id}/`).then((res) => res);
+
 const sample2: VFC = () => {
   // 画面表示用のタスク一覧
-  const [tasks, setTasks] = useState<Task[]>([]);
+  const { data: tasks, mutate } = useSWR<Task[]>(URL, taskGet);
   // 画面入力
-  const [editTask, setEditTask] = useState<Task>(initTask);
+  const [editTask, setEditTask] = useState<EditTask>(initTask);
 
   // =============================================
   // 追加・更新ボタンクリック
   //    画面入力値をタスク一覧へ反映する
-  const handleInsertClick = () => {
+  const handleInsertClick = async () => {
     if (editTask.id === 0) {
       // id が 0 の場合は追加時
-      editTask.id = tasks.length + 1;
-      // タスクデータへ追加する
-      setTasks([...tasks, editTask]);
+      await taskPost(URL, editTask);
+      await mutate();
     } else {
-      // id が 0以外 の場合は更新時
-      const tmpTasks = tasks.map((task) => {
-        // タスクデータの中から更新対象を入力値で入れ替える
-        if (task.id !== editTask.id) {
-          return task;
-        } else {
-          return editTask;
-        }
-      });
-      // タスクデータへ反映する
-      setTasks(tmpTasks);
+      // 更新
+      await taskPut(URL, editTask.id, editTask);
+      await mutate();
     }
     // 画面入力用のステートを初期化する
     setEditTask(initTask);
   };
   // =============================================
   // 削除ボタンクリック
-  const handleDeleteClick = (id: number) => {
-    // タスクデータから削除対象のタスクを除く
-    const tmpTasks = tasks.filter((task) => {
-      return task.id !== id;
-    });
-    // タスクデータへ追加する
-    setTasks(tmpTasks);
+  const handleDeleteClick = async (id: number) => {
+    // タスクデータを削除する
+    await taskDelete(URL, id);
+    await mutate();
   };
 
   // =============================================
@@ -83,35 +94,35 @@ const sample2: VFC = () => {
       </div>
       {/* タスク一覧 */}
       <div className="flex flex-col">
-        {tasks.map((task) => {
-          return (
-            <div className="flex flex-row ">
-              {/* タスクデータ */}
-              <div key={task.id} className="mr-10">
-                {task.task}
+        {tasks &&
+          tasks.map((task) => {
+            // return <span key={task.id}>{task.task}</span>;
+            return (
+              <div className="flex flex-row key={task.id}">
+                {/* タスクデータ */}
+                <div className="mr-10">{task.task}</div>
+                {/* 削除ボタン */}
+                <button
+                  className="m-6 bg-gray-300"
+                  onClick={() => {
+                    handleDeleteClick(task.id);
+                  }}
+                >
+                  削除
+                </button>
+                {/* 更新ボタン */}
+                {/* 該当行のタスクを入力用のテキストボックスに表示させる */}
+                <button
+                  className="m-6 bg-gray-300"
+                  onClick={() => {
+                    setEditTask(task);
+                  }}
+                >
+                  更新
+                </button>
               </div>
-              {/* 削除ボタン */}
-              <button
-                className="m-6 bg-gray-300"
-                onClick={() => {
-                  handleDeleteClick(task.id);
-                }}
-              >
-                削除
-              </button>
-              {/* 更新ボタン */}
-              {/* 該当行のタスクを入力用のテキストボックスに表示させる */}
-              <button
-                className="m-6 bg-gray-300"
-                onClick={() => {
-                  setEditTask(task);
-                }}
-              >
-                更新
-              </button>
-            </div>
-          );
-        })}
+            );
+          })}
       </div>
     </div>
   );
