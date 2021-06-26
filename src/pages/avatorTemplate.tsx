40a28b63b21a3255bdfb72f81f9d010ebb73f966
oshita-n　　 <oshita-n@whispon.com>
2021-06-26 21:32:29 +0900

    🔨 ボタンを押したらカメラの画面に遷移させる方法が分からない。

--- src/pages/avatorTemplate.tsx
+++ src/pages/avatorTemplate.tsx
@@ -3,14 +3,30 @@ import { Divider } from "../components/shared/Divider";
 import { MainLayouts } from "../layouts/MainLayouts";
 import { FittingColor } from "../components/shared/FittingColor";
 import { Menu, Transition } from '@headlessui/react'
-import { Fragment, useEffect, useRef, useState } from 'react'
+import { Fragment, useCallback, useEffect, useRef, useState } from 'react'
 import { CameraIcon } from '@heroicons/react/solid'
 import { PhotographIcon } from '@heroicons/react/solid'
 import Webcam from "react-webcam";
-
+import React from "react";
 
 export const avatorTemplate: NextPage = () => {
+  const videoConstraints = {
+    width: 1280,
+    height: 720,
+    facingMode: "user"
+  };
+  const webcamRef = React.useRef(null);
+  const [image, setImage] = useState()
+  const capture = React.useCallback(
+    () => {
+      setImage(webcamRef.current.getScreenshot());
+    },
+    [webcamRef]
+  );
+
   return (
+
+    
     <div>
       <MainLayouts title="top">
         <div className="flex flex-col items-center">
@@ -36,30 +52,27 @@ export const avatorTemplate: NextPage = () => {
                 <Menu.Items className="absolute right-0 w-56 mt-2 origin-top-right bg-white divide-y divide-gray-100 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                   <div className="px-1 py-1 ">
                     <Menu.Item>
-                      {({ active }) => (
-                        <button
-                          className="text-gray-900 flex rounded-md items-center w-full px-2 py-2 text-sm"
-                        >
-                            <CameraIcon
-                              className="w-5 h-5 mr-2"
-                              aria-hidden="true"
-                            />
-                          写真を撮る
-                        </button>
-                      )}
+                      <button
+                        className="text-gray-900 flex rounded-md items-center w-full px-2 py-2 text-sm"
+                        onClick = { capture }
+                      >
+                          <CameraIcon
+                            className="w-5 h-5 mr-2"
+                            aria-hidden="true"
+                          />
+                        写真を撮る
+                      </button>
                     </Menu.Item>
                     <Menu.Item>
-                      {({ active }) => (
-                        <button
-                          className="text-gray-900 flex rounded-md items-center w-full px-2 py-2 text-sm"
-                        >
-                            <PhotographIcon
-                              className="w-5 h-5 mr-2"
-                              aria-hidden="true"
-                            />
-                          フォトライブラリ
-                        </button>
-                      )}
+                      <button
+                        className="text-gray-900 flex rounded-md items-center w-full px-2 py-2 text-sm"
+                      >
+                          <PhotographIcon
+                            className="w-5 h-5 mr-2"
+                            aria-hidden="true"
+                          />
+                        フォトライブラリ
+                      </button>
                     </Menu.Item>
                   </div>                 
                 </Menu.Items>
@@ -68,8 +81,20 @@ export const avatorTemplate: NextPage = () => {
           </div>
         </div>
       </MainLayouts>
+      <Webcam
+          audio={false}
+          height={980}
+          ref={webcamRef}
+          screenshotFormat="image/jpeg"
+          width={480}
+          videoConstraints={videoConstraints}
+        />
+        <button onClick={ capture }>Capture photo</button>
+        {/* <img src={ image } /> */}
     </div>
   );
 };
+
+
 
 export default avatorTemplate;
