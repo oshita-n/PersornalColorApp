dc7bbc64ee089d105a14f72f3f11ecf37ef4da51
oshita-n　　 <oshita-n@whispon.com>
2021-07-03 21:02:46 +0900

    🔨 写真を撮れるところまで

--- src/pages/avatorTemplate.tsx
+++ src/pages/avatorTemplate.tsx
@@ -2,7 +2,7 @@ import { NextPage } from "next";
 import { Divider } from "../components/shared/Divider";
 import { MainLayouts } from "../layouts/MainLayouts";
 import { FittingColor } from "../components/shared/FittingColor";
-import { Menu, Transition } from '@headlessui/react'
+import { Menu, Transition, Dialog } from '@headlessui/react'
 import { Fragment, useCallback, useEffect, useRef, useState } from 'react'
 import { CameraIcon } from '@heroicons/react/solid'
 import { PhotographIcon } from '@heroicons/react/solid'
@@ -23,6 +23,16 @@ export const avatorTemplate: NextPage = () => {
     },
     [webcamRef]
   );
+
+  let [isOpen, setIsOpen] = useState(false)
+
+  function closeModal() {
+    setIsOpen(false)
+  }
+
+  function openModal() {
+    setIsOpen(true)
+  }
 
   return (
 
@@ -54,11 +64,12 @@ export const avatorTemplate: NextPage = () => {
                     <Menu.Item>
                       <button
                         className="text-gray-900 flex rounded-md items-center w-full px-2 py-2 text-sm"
-                        onClick = { capture }
+                        onClick={openModal}
                       >
                           <CameraIcon
                             className="w-5 h-5 mr-2"
                             aria-hidden="true"
+                            
                           />
                         写真を撮る
                       </button>
@@ -81,20 +92,75 @@ export const avatorTemplate: NextPage = () => {
           </div>
         </div>
       </MainLayouts>
-      <Webcam
-          audio={false}
-          height={980}
-          ref={webcamRef}
-          screenshotFormat="image/jpeg"
-          width={480}
-          videoConstraints={videoConstraints}
-        />
-        <button onClick={ capture }>Capture photo</button>
-        {/* <img src={ image } /> */}
+        <Transition appear show={isOpen} as={Fragment}>
+        <Dialog
+          as="div"
+          className="fixed inset-0 z-10 overflow-y-auto"
+          onClose={closeModal}
+        >
+          <div className="min-h-screen px-4 text-center">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <Dialog.Overlay className="fixed inset-0" />
+            </Transition.Child>
+
+            {/* This element is to trick the browser into centering the modal contents. */}
+            <span
+              className="inline-block h-screen align-middle"
+              aria-hidden="true"
+            >
+              &#8203;
+            </span>
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+            >
+              <div className="inline-block w-full max-w-md p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-2xl">
+                
+              <Webcam
+                audio={false}
+                height={980}
+                ref={webcamRef}
+                screenshotFormat="image/jpeg"
+                width={480}
+                videoConstraints={videoConstraints}
+              /> 
+                <div className="mt-4">
+                  <button
+                      type="button"
+                      className="inline-flex justify-center px-4 py-2 text-sm font-medium text-blue-900 bg-blue-100 border border-transparent rounded-md hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500"
+                      onClick={capture}
+                    >
+                      Take a picture！
+                  </button>
+                  <button
+                    type="button"
+                    className="inline-flex justify-center px-4 py-2 text-sm font-medium text-blue-900 bg-blue-100 border border-transparent rounded-md hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500"
+                    onClick={closeModal}
+                  >
+                    Got it, thanks!
+                  </button>
+                </div>
+              </div>
+            </Transition.Child>
+          </div>
+        </Dialog>
+      </Transition>
     </div>
+    
   );
 };
-
-
 
 export default avatorTemplate;
