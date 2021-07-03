d1538b5f61af32dc4fc3b2d953f5f7ddb25868de
taekowithrowgit <taeko.forest@gmail.com>
2021-07-03 17:44:48 +0900

    質問4回後、結果に遷移する動きと、質問結果のページを一部作成

--- src/pages/questionResult.tsx
+++ src/pages/questionResult.tsx
@@ -1,7 +1,27 @@
-import { NextPage } from "next";
+import { NextPage } from 'next';
+import { Divider } from '../components/shared/Divider';
+import { MainLayouts } from '../layouts/MainLayouts';
+import { Button } from '../components/shared/Button';
 
-export const questionResult: NextPage = () => {
-  return <div>questionResult</div>;
+const questionResult: NextPage = () => {
+  return (
+    <MainLayouts title='Top'>
+      <div className='flex flex-col items-center'>
+        <span>あなたのパーソナルカラーは"{}"です</span>
+        <div className='flex flex-col my-4'>
+          <div className='flex space-x-10'>
+            <Button button bgColor='white' textColor='black' className='w-32'>
+              自分の顔とカラーを比較する
+            </Button>
+            <Button button bgColor='white' textColor='black' className='w-32'>
+              カラーパレットを見る
+            </Button>
+          </div>
+        </div>
+        <Divider />
+      </div>
+    </MainLayouts>
+  );
 };
 
 export default questionResult;
